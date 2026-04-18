0b71defd0b1554d67280fdb09f427f641b8735ba
Muhammad Umer <mu249271@gmail.com>
2026-04-18 11:44:38 +0500

    update port for render requirements

--- DB_Project/DB_Project-main/ecommerce-backend/app.js
+++ DB_Project/DB_Project-main/ecommerce-backend/app.js
@@ -78,9 +78,9 @@ app.get('/admin',/* authenticateToken, authorizeRole('admin'),*/ (req, res) => {
     res.json({ message: `Welcome, admin !` });
 });//${req.user.email}
 
-app.listen(5000,'0.0.0.0', ()=>{
-    console.log("App is listening on port 5000")
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,'0.0.0.0', ()=>{
+    console.log(`App is listening on port ${PORT}`)
 })
-
 
 
